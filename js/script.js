6338363fac11be7f9eedbd3983a4e728b303a7bb
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-08-13 11:32:53 -0700

    Add displayBooks function

--- js/script.js
+++ js/script.js
@@ -23,3 +23,45 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+function displayBooks() {
+    for (let book of myLibrary) {
+        const tableBody = document.querySelector("tbody");
+        const tableRow = document.createElement("tr");
+        const title = document.createElement("td");
+        const author = document.createElement("td");
+        const pages = document.createElement("td");
+        const read = document.createElement("td");
+        const toggleReadCell = document.createElement("td");
+        const readBtn = document.createElement("button");
+        const deleteCell = document.createElement("td");
+        const deleteBtn = document.createElement("button");
+        
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        read.textContent = book.read;
+        readBtn.textContent = "Toggle Read";
+        deleteBtn.textContent = "Delete"
+
+        toggleReadCell.appendChild(readBtn);
+        deleteCell.appendChild(deleteBtn);
+        tableRow.appendChild(title);
+        tableRow.appendChild(author);
+        tableRow.appendChild(pages);
+        tableRow.appendChild(read);
+        tableRow.appendChild(toggleReadCell);
+        tableRow.appendChild(deleteCell);
+        tableBody.appendChild(tableRow);
+    }
+}
+
+// pre-existing data
+let book1 = new Book("Limited Wish", "Mark Lawrence", 222, true);
+let book2 = new Book("The Light of All That Falls", "James Islington", 864, true);
+let book3 = new Book("All Systems Red", "Martha Wells", 144, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+
+displayBooks();
